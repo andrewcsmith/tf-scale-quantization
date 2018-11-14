98622501a91bbefbe88635002fef498b320a17ef
Andrew C. Smith <andrewchristophersmith@gmail.com>
2018-11-14 08:44:42 -0800

    plot over the course of 4 octaves

--- plot_1d.py
+++ plot_1d.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from tensorflow.python import debug as tf_debug
 
 c = 0.05
-n_points = 1001
+n_points = 2048
 
 # In sequence at n_points = 101
 # TotalSeconds      : 16.0295673
@@ -24,8 +24,8 @@ y_op = calc_func_graph(log_pitches, vectors, c=c)
 
 sess.run(init_op)
 
-xs = np.linspace(0.0, 1.0, n_points)
-ys = sess.run([log_pitches.assign(tf.expand_dims(xs, 1)), y_op])[1]
+xs = np.linspace(0.0, 4.0, n_points)
+_, ys = sess.run([log_pitches.assign(xs[:, None]), y_op])
 # The following iterates over each element in sequence, rather than loading it
 # all as a single parallel graph.
 # ys = np.array([sess.run([log_pitches.assign([x]), y_op])[1] for x in xs])
